8b8537e6a732b5c6f83106e72f26f6e08e0b5353
EternalTLD <shvejkin01@mail.ru>
2023-07-18 13:25:30 +0300

    question admin update

--- ask_project/questions/admin.py
+++ ask_project/questions/admin.py
@@ -1,10 +1,25 @@
 from django.contrib import admin
-
 from .models import Category, Tag, Question, Answer, QuestionImages
 
 
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ['title', 'category', 'author', 'date_published', 'draft']
+    list_filter = ['draft', 'category', 'date_published', 'date_created', 'author']
+    search_fields = ['title', 'content']
+    prepopulated_fields = {'slug': ('title')}
+    raw_id_fields = ['author']
+    date_hierarchy = 'date_published'
+    ordering = ['-draft', 'date_published']
+
+class AnswerAdmin(admin.ModelAdmin):
+    list_display = ['question', 'author', 'date_published', 'best_answer']
+    list_filter = ['question', 'author', 'date_published', 'best_answer', ]
+    search_fields = ['question', 'content']
+    raw_id_fields = ['author']
+    date_hierarchy = 'date_published'
+    ordering = ['date_published']
+
 admin.site.register(Category)
-admin.site.register(Question)
 admin.site.register(Tag)
-admin.site.register(Answer)
 admin.site.register(QuestionImages)
