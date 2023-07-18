1dfcc9463b13fb968c6018e11c6afc93b0462b93
EternalTLD <shvejkin01@mail.ru>
2023-07-18 13:30:52 +0300

    question admin update

--- ask_project/questions/admin.py
+++ ask_project/questions/admin.py
@@ -7,11 +7,12 @@ class QuestionAdmin(admin.ModelAdmin):
     list_display = ['title', 'category', 'author', 'date_published', 'draft']
     list_filter = ['draft', 'category', 'date_published', 'date_created', 'author']
     search_fields = ['title', 'content']
-    prepopulated_fields = {'slug': ('title')}
+    prepopulated_fields = {'slug': ('title',)}
     raw_id_fields = ['author']
     date_hierarchy = 'date_published'
     ordering = ['-draft', 'date_published']
 
+@admin.register(Answer)
 class AnswerAdmin(admin.ModelAdmin):
     list_display = ['question', 'author', 'date_published', 'best_answer']
     list_filter = ['question', 'author', 'date_published', 'best_answer', ]
