7fe82815e127382c0da7ffd22bc5cfcf97b7d95d
EternalTLD <shvejkin01@mail.ru>
2023-07-18 13:13:49 +0300

    questions models update

--- ask_project/questions/models.py
+++ ask_project/questions/models.py
@@ -1,14 +1,19 @@
 from django.db import models
 from django.urls import reverse
+from django.utils import timezone
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
 class Category(models.Model):
     """Categories"""
-    name = models.CharField(max_length=50, unique=True, verbose_name='Category')
-    image = models.ImageField(null=True, upload_to='categories/', verbose_name='Image')
+    name = models.CharField(max_length=50, unique=True, verbose_name='Категория')
+    image = models.ImageField(null=True, upload_to='categories/', verbose_name='Картинка')
     slug = models.SlugField(max_length=160, unique=True)
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
 
     def __str__(self) -> str:
         return self.name
@@ -16,14 +21,14 @@ class Category(models.Model):
     def get_absolute_url(self):
         return reverse('questions:by_category', kwargs={'slug': self.slug})
 
-    class Meta:
-        verbose_name = 'Category'
-        verbose_name_plural = 'Categories'
-
 class Tag(models.Model):
     """Tags"""
-    title = models.CharField(max_length=20, unique=True, verbose_name='Tag')
+    title = models.CharField(max_length=20, unique=True, verbose_name='Тег')
     slug = models.SlugField(max_length=160, unique=True, default=title)
+
+    class Meta:
+        verbose_name = 'Тег'
+        verbose_name_plural = 'Теги'
     
     def __str__(self) -> str:
         return self.title
@@ -31,61 +36,64 @@ class Tag(models.Model):
     def get_absolute_url(self):
         return reverse('questions:by_tag', kwargs={'slug': self.slug})
 
-    class Meta:
-        verbose_name = 'Tag'
-        verbose_name_plural = 'Tags'
-
 class Question(models.Model):
     """Questions"""
-    title = models.CharField(max_length=50, verbose_name='Title')
+    title = models.CharField(max_length=50, verbose_name='Заголовок')
     category = models.ForeignKey(
         Category, 
         on_delete=models.PROTECT, 
-        verbose_name='Category', 
+        verbose_name='Категория', 
         related_name='category'
     )
-    author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='User')
-    date_published = models.DateField(verbose_name='Publication date')
-    content = models.TextField(verbose_name='Question')
-    likes = models.IntegerField(default=0, verbose_name='Likes')
-    dislikes = models.IntegerField(default=0, verbose_name='Dislikes')
-    viewes = models.IntegerField(default=0, verbose_name='Views')
-    tags = models.ManyToManyField(Tag, verbose_name='Tag', related_name='tags')
-    draft = models.BooleanField(default=False, verbose_name='Draft')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Автор')
+    date_published = models.DateTimeField(default=timezone.now, verbose_name='Дата публикации')
+    date_created = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    date_updated = models.DateTimeField(auto_now=True, verbose_name='Дата изменения')
+    content = models.TextField(verbose_name='Содержание')
+    likes = models.IntegerField(default=0, verbose_name='Лайки')
+    dislikes = models.IntegerField(default=0, verbose_name='Дизлайки')
+    viewes = models.IntegerField(default=0, verbose_name='Просмотры')
+    tags = models.ManyToManyField(Tag, verbose_name='Теги', related_name='tags')
+    draft = models.BooleanField(default=False, verbose_name='Черновик')
     slug = models.SlugField(max_length=160, unique=True)
+
+    class Meta:
+        ordering = ['-date_published']
+        indexes = [
+            models.Index(fields=['-date_published']),
+        ]
+        verbose_name = 'Вопрос'
+        verbose_name_plural = 'Вопросы'
 
     def __str__(self) -> str:
         return self.title
     
     def get_absolute_url(self):
         return reverse('questions:question_detail', kwargs={'slug': self.slug})
-
-    
-    class Meta:
-        ordering = ['-date_published']
-        verbose_name = 'Question'
-        verbose_name_plural = 'Questions'
     
 class Answer(models.Model):
     """Answers"""
-    question = models.ForeignKey(Question, on_delete=models.CASCADE, verbose_name='Question')
-    author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Author')
-    content = models.TextField(verbose_name='Answer')
-    date_published = models.DateField(verbose_name='Publication date')
-    likes = models.IntegerField(default=0, verbose_name='Likes')
-    dislikes = models.IntegerField(default=0, verbose_name='Dislikes')
-    best_answer = models.BooleanField(default=False, verbose_name='Best answer')
+    question = models.ForeignKey(Question, on_delete=models.CASCADE, verbose_name='Вопрос')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Автор')
+    content = models.TextField(verbose_name='Содержание')
+    date_published = models.DateField(verbose_name='Дата публикации')
+    likes = models.IntegerField(default=0, verbose_name='Лайки')
+    dislikes = models.IntegerField(default=0, verbose_name='Дизлайки')
+    best_answer = models.BooleanField(default=False, verbose_name='Лучший ответ')
 
     class Meta:
-        ordering = ['likes']
-        verbose_name = 'Answer'
-        verbose_name_plural = 'Answers'
+        ordering = ['-likes']
+        indexes = [
+            models.Index(fields=['-likes'])
+        ]
+        verbose_name = 'Ответ'
+        verbose_name_plural = 'Ответы'
     
 class QuestionImages(models.Model):
     """Additional images"""
-    question = models.ForeignKey(Question, on_delete=models.CASCADE, verbose_name='Question')
-    image = models.ImageField(blank=True, upload_to='questions_images/', verbose_name='Image')
+    question = models.ForeignKey(Question, on_delete=models.CASCADE, verbose_name='Вопрос')
+    image = models.ImageField(blank=True, upload_to='questions_images/', verbose_name='Картинка')
 
     class Meta:
-        verbose_name = 'Question image'
-        verbose_name_plural = 'Question images'
+        verbose_name = 'Прикрепленная картинка'
+        verbose_name_plural = 'Прикрепленные картинки'
