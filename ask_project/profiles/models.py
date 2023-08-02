daabafbfbb0da72d98d5a8599839d454cac05b15
EternalTLD <shvejkin01@mail.ru>
2023-08-02 16:10:34 +0300

    add profile model

--- ask_project/profiles/models.py
+++ ask_project/profiles/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+from users.models import User
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    profile_image = models.ImageField(upload_to='avatars/', blank=True, verbose_name='аватар')
+    rating = models.IntegerField(default=0, verbose_name='Рейтинг')
+    city = models.CharField(max_length=25, blank=True, verbose_name='Город')
+    country = models.CharField(max_length=25, blank=True, verbose_name='Страна')
+
+    class Meta:
+        verbose_name = 'Профиль'
+        verbose_name_plural = 'Профили'
+
+    def __str__(self) -> str:
+        return f'Профиль пользователя {self.user.username}'
+
