ce97b2dbf69c06da46d0c70d3858a222751f20df
EternalTLD <shvejkin01@mail.ru>
2023-08-03 16:42:45 +0300

    update profile detail

--- ask_project/profiles/views.py
+++ ask_project/profiles/views.py
@@ -1,8 +1,11 @@
+from typing import Any, Dict
 from django.shortcuts import render
+from django.core.paginator import Paginator
 from django.views.generic.detail import DetailView
 
 from .forms import UserEditForm, ProfileEditForm
 from users.models import User
+from questions.models import Question
 
 class ProfileDetailView(DetailView):
     template_name = 'profiles/profile_detail.html'
@@ -10,6 +13,22 @@ class ProfileDetailView(DetailView):
     slug_field = 'username'
     slug_url_kwarg = 'username'
     context_object_name = 'user'
+
+    def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
+        context = super(DetailView, self).get_context_data(**kwargs)
+        user_questions = self.get_user_questions()
+        context['user_questions'] = user_questions
+        context['page_obj'] = user_questions
+        return context
+    
+    def get_user_questions(self):
+        user = self.get_object()
+        user_questions = Question.objects.filter(author=user)
+        paginator = Paginator(user_questions, 3)
+        page = self.request.GET.get('page')
+        page_obj = paginator.get_page(page)
+        return page_obj
+
 
 def profile_edit_view(request):
 
