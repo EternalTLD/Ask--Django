8a02fc967920c7713e9f2ef6d5dcdc3680c86e45
EternalTLD <shvejkin01@mail.ru>
2023-08-07 18:15:03 +0300

    add gitignore

--- ask_project/profiles/views.py
+++ ask_project/profiles/views.py
@@ -41,8 +41,8 @@ def profile_edit_view(request):
     if request.method == 'POST':
         user_form = UserEditForm(instance=request.user, data=request.POST)
         profile_form = ProfileEditForm(instance=request.user.profile, 
-                                  data=request.POST, 
-                                  files=request.FILES)
+                                       data=request.POST, 
+                                       files=request.FILES)
         if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
             profile_form.save()
