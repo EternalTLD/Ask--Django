97e7c52eb954bec0d37b70afbb6ad944cdfe095b
EternalTLD <shvejkin01@mail.ru>
2023-08-08 20:04:08 +0300

    add like/dislike urls

--- ask_project/questions/urls.py
+++ ask_project/questions/urls.py
@@ -7,7 +7,42 @@ urlpatterns = [
     path('search/', question_search_view, name='question_search'),
     path('tags/<slug:tag_slug>/', QuestionsByTagView.as_view(), name='by_tag'),
     path('new_question/', NewQuestionView.as_view(), name='new_question'),
-    path('vote/<str:action>/<int:id>/', add_question_vote_view, name='question_vote'),
-    path('<int:id>/<slug:slug>/', QuestionView.as_view(), name='question_detail'),
+    path(
+        'question/like/<int:pk>/', 
+        VoteView.as_view(
+            model=Question, 
+            vote_model=QuestionVote, 
+            vote_type=QuestionVote.LIKE
+        ), 
+        name='question_like'
+    ),
+    path(
+        'question/dislike/<int:pk>/', 
+        VoteView.as_view(
+            model=Question, 
+            vote_model=QuestionVote, 
+            vote_type=QuestionVote.DISLIKE
+        ), 
+        name='question_dislike'
+    ),
+    path(
+        'answer/like/<int:pk>/', 
+        VoteView.as_view(
+            model=Answer, 
+            vote_model=AnswerVote, 
+            vote_type=AnswerVote.LIKE
+        ), 
+        name='answer_like'
+    ),
+    path(
+        'answer/dislike/<int:pk>/', 
+        VoteView.as_view(
+            model=Answer, 
+            vote_model=AnswerVote, 
+            vote_type=AnswerVote.DISLIKE
+        ), 
+        name='answer_dislike'
+    ),
+    path('<int:pk>/<slug:slug>/', QuestionView.as_view(), name='question_detail'),
     path('', QuestionListView.as_view(), name='home'),
 ]
