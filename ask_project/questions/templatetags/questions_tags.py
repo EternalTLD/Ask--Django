3372899d685f685c03947cff82d0d47a87e4c267
EternalTLD <shvejkin01@mail.ru>
2023-08-08 23:54:39 +0300

    add likes/dislikes tags

--- ask_project/questions/templatetags/questions_tags.py
+++ ask_project/questions/templatetags/questions_tags.py
@@ -3,7 +3,7 @@ from django.db.models import Count
 from django.utils import timezone
 
 from taggit.models import Tag
-from ..models import Question
+from ..models import Question, Answer
 
 
 register = template.Library()
@@ -17,3 +17,19 @@ def get_popular_tags():
 def get_todays_popular_questions():
     today = timezone.now().date()
     return Question.published.filter(date_published__date=today).order_by('-views')[:5]
+
+@register.filter
+def count_question_likes(question_id):
+    return Question.objects.filter(pk=question_id, questionvote__vote=1).count()
+
+@register.filter
+def count_question_dislikes(question_id):
+    return Question.objects.filter(pk=question_id, questionvote__vote=-1).count()
+
+@register.filter
+def count_answer_likes(answer_id):
+    return Answer.objects.filter(pk=answer_id, answervote__vote=1).count()
+
+@register.filter
+def count_answer_dislikes(answer_id):
+    return Answer.objects.filter(pk=answer_id, answervote__vote=-1).count()
