75eb1c553658fd407a5e60dfe9f0ee4f00f0469b
EternalTLD <shvejkin01@mail.ru>
2023-08-13 19:15:50 +0300

    fixed popular tags

--- ask_project/questions/templatetags/questions_tags.py
+++ ask_project/questions/templatetags/questions_tags.py
@@ -11,7 +11,7 @@ register = template.Library()
 @register.simple_tag
 def get_popular_tags():
     return Tag.objects.filter(question__draft=False).annotate(
-        total_questions=Count('question')).order_by('-total_questions')[:10]
+        total_questions=Count('question')).order_by('-total_questions')[:20]
 
 @register.simple_tag
 def get_todays_popular_questions():
