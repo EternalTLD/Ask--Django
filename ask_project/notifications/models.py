cd7af23ec72e0c34436e0228b91687531e054f14
EternalTLD <shvejkin01@mail.ru>
2023-08-28 00:46:17 +0300

    fixed notifications model and manager

--- ask_project/notifications/models.py
+++ ask_project/notifications/models.py
@@ -11,18 +11,20 @@ class NotificationManager(models.Manager):
     
     def recieved(self, to_user):
         qs = Notification.objects.filter(to_user=to_user)
-        notifications = list(qs)
-        return notifications
+        return qs
+    
+    def sent(self, from_user):
+        qs = Notification.objects.filter(from_user=from_user)
+        return qs
     
     def unread(self, to_user):
-        qs = Notification.objects.filter(to_user=to_user, read_at=False)
-        notifications = list(qs)
-        return notifications
+        qs = Notification.objects.filter(to_user=to_user, read_at=None)
+        return qs
     
     def mark_all_as_read(self, to_user):
         qs = Notification.objects.unread(to_user=to_user)
-        qs.update(read_at=timezone.now)
-        return True
+        qs.update(read_at=timezone.now())
+        return qs
 
 class Notification(models.Model):
     TYPES = (
@@ -33,18 +35,18 @@ class Notification(models.Model):
     from_user = models.ForeignKey(
         User, 
         on_delete=models.CASCADE,
-        related_name='notifications', 
-        verbose_name='От пользователя'
+        related_name='sent_notifications', 
+        verbose_name='Отправитель',
     )
     to_user = models.ForeignKey(
         User, 
         on_delete=models.CASCADE,
-        related_name='_notifications', 
-        verbose_name='К пользователю'
+        related_name='recieved_notifications', 
+        verbose_name='Получатель'
     )
     question = models.ForeignKey(Question, on_delete=models.CASCADE, verbose_name='Вопрос')
     created_at = models.DateTimeField(default=timezone.now, verbose_name='Отправлено')
-    read_at = models.DateTimeField(default=None, verbose_name='Прочитано')
+    read_at = models.DateTimeField(default=None, null=True, blank=True, verbose_name='Прочитано')
     type = models.CharField(max_length=2, choices=TYPES)
 
     objects = NotificationManager()
@@ -53,20 +55,19 @@ class Notification(models.Model):
         verbose_name = 'Уведомление'
         verbose_name_plural = 'Уведомления'
         ordering = ['-created_at']
-        unique_together = ('from_user', 'to_user')
 
     def __str__(self) -> str:
         if self.type == 'QL':
-            return f'Пользователю {self.from_user.username} понравился вопрос {self.question.pk}'
+            return f'Пользователю {self.from_user.username} понравился ваш вопрос'
         elif self.type == 'AL':
-            return f'Пользователю {self.from_user.username} понравился ответ на вопрос {self.question.pk}'
+            return f'Пользователю {self.from_user.username} понравился ваш ответ на вопрос'
         elif self.type == 'AQ':
-            return f'Пользователь {self.from_user.username} ответил на вопрос {self.question.pk}'
+            return f'Пользователь {self.from_user.username} ответил на ваш вопрос'
         
     def timesince(self):
         pass
     
     def mark_as_read(self):
-        self.read_at = timezone.now
+        self.read_at = timezone.now()
         self.save()
         return True
