be1e28ed977da1eef0394660873f5a1b11116236
EternalTLD <shvejkin01@mail.ru>
2023-08-28 00:46:47 +0300

    add admin notifications

--- ask_project/notifications/admin.py
+++ ask_project/notifications/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Notification
+
+
+@admin.register(Notification)
+class NotificationAdmin(admin.ModelAdmin):
+    raw_id_fields = ('to_user', 'from_user')
+    list_display = ('to_user', 'from_user',
+                    'question', 'created_at', 'read_at', 'type')
