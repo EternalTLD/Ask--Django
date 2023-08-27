a5827b98bde8d7ae62d054fcc5c65250398672e8
EternalTLD <shvejkin01@mail.ru>
2023-08-27 17:49:51 +0300

    rename questions urls

--- ask_project/questions/urls.py
+++ ask_project/questions/urls.py
@@ -1,11 +1,16 @@
 from django.urls import path, include
-from .views import *
+
+from .views import (QuestionsListView, QuestionsByTagListView, PopularQuestionsListView,
+                    QuestionCreateView, QuestionUpdateView, QuestionDeleteView,
+                    QuestionView, VoteView, question_search_view)
+from .models import Question, QuestionVote, Answer, AnswerVote
 
 app_name = 'questions'
 
 urlpatterns = [
     path('search/', question_search_view, name='question_search'),
-    path('tags/<slug:tag_slug>/', QuestionsByTagView.as_view(), name='by_tag'),
+    path('tags/<slug:tag_slug>/', QuestionsByTagListView.as_view(), name='by_tag'),
+    path('popular/', PopularQuestionsListView.as_view(), name='popular_questions'),
     path('question/create/', QuestionCreateView.as_view(), name='question_create'),
     path(
         'question/like/<int:pk>/', 
@@ -46,5 +51,5 @@ urlpatterns = [
     path('<int:pk>/<slug:slug>/', QuestionView.as_view(), name='question_detail'),
     path('update/<int:pk>/', QuestionUpdateView.as_view(), name="question_update"),
     path('delete/<int:pk>/', QuestionDeleteView.as_view(), name='question_delete'),
-    path('', QuestionListView.as_view(), name='home'),
+    path('', QuestionsListView.as_view(), name='home'),
 ]
