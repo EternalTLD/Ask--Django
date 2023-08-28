f9f6876c334eb0349391186008dae23776f20a06
EternalTLD <shvejkin01@mail.ru>
2023-08-28 23:28:39 +0300

    add profiles template tags

---
+++ ask_project/profiles/templatetags/profiles_tags.py
@@ -0,0 +1,35 @@
+from django import template
+from django.db.models import Count
+from django.utils import timezone
+from django.contrib.auth import get_user_model
+
+from questions.models import Question, Answer, QuestionVote, AnswerVote
+
+
+User = get_user_model()
+
+register = template.Library()
+
+@register.filter
+def count_user_questions(user_id):
+    return Question.published.filter(author=user_id).count()
+
+@register.filter
+def count_user_answers(user_id):
+    return Answer.objects.filter(author=user_id).count()
+
+@register.filter
+def count_user_recieved_likes(user_id):
+    questions = Question.published.filter(author=user_id)
+    question_likes = QuestionVote.objects.filter(vote_target__in=questions, vote=1).count()
+    answers = Answer.objects.filter(author=user_id)
+    answer_likes = AnswerVote.objects.filter(vote_target__in=answers, vote=1).count()
+    return question_likes + answer_likes
+
+@register.filter
+def count_user_recieved_dislikes(user_id):
+    questions = Question.published.filter(author=user_id)
+    question_dislikes = QuestionVote.objects.filter(vote_target__in=questions, vote=-1).count()
+    answers = Answer.objects.filter(author=user_id)
+    answer_dislikes = AnswerVote.objects.filter(vote_target__in=answers, vote=-1).count()
+    return question_dislikes + answer_dislikes
