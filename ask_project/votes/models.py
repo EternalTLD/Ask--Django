a2e3089fe2275fb03e5f0f4e0f9a73e0e5ee17eb
EternalTLD <shvejkin01@mail.ru>
2023-09-22 22:47:05 +0300

    add vote manager

--- ask_project/votes/models.py
+++ ask_project/votes/models.py
@@ -1,11 +1,19 @@
 from django.db import models
-from django.contrib.auth import get_user_model
 from django.utils import timezone
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
 
+from ask_project import settings
 
-User = get_user_model()
+
+class VoteManager(models.Manager):
+    def count_likes(self):
+        likes = self.get_queryset().filter(vote=1).count()
+        return likes
+
+    def count_dislikes(self):
+        dislikes = self.get_queryset().filter(vote=-1).count()
+        return dislikes
 
 class Vote(models.Model):
     LIKE = 1
@@ -16,8 +24,12 @@ class Vote(models.Model):
         (DISLIKE, 'dislike')
     )
     vote = models.SmallIntegerField(choices=VOTES, verbose_name='Голос')
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, 
+        on_delete=models.CASCADE, 
+        verbose_name='Пользователь'
+    )
     timestamp = models.DateTimeField(default=timezone.now, verbose_name='Дата')
-    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Пользователь')
 
     content_type = models.ForeignKey(
         ContentType, 
@@ -26,3 +38,5 @@ class Vote(models.Model):
     )
     object_id = models.PositiveIntegerField()
     content_object = GenericForeignKey()
+
+    objects = VoteManager()
