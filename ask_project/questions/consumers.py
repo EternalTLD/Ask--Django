a2b18d1b8a52e2797a23c19c1646d810af38a3e1
EternalTLD <shvejkin01@mail.ru>
2023-09-22 22:53:11 +0300

    add answer notifications

--- ask_project/questions/consumers.py
+++ ask_project/questions/consumers.py
@@ -1,42 +1,47 @@
 import json
 
-from channels.generic.websocket import AsyncWebsocketConsumer
-from channels.db import database_sync_to_async
+from django.contrib.contenttypes.models import ContentType
+from channels.generic.websocket import WebsocketConsumer
+from asgiref.sync import async_to_sync
 
+from notifications.models import Notification
 from .models import Question, Answer
 
 
-class AnswersConsumer(AsyncWebsocketConsumer):
+class AnswersConsumer(WebsocketConsumer):
 
-    async def connect(self):
+    def connect(self):
         self.question_id = self.scope['url_route']['kwargs']['question_id']
         self.question_group_name = f'question_{self.question_id}'
 
-        await self.channel_layer.group_add(
+        async_to_sync(self.channel_layer.group_add)(
             self.question_group_name,
             self.channel_name
         )
         
-        await self.accept()
+        self.accept()
 
-    async def disconnect(self, code):
-        await self.channel_layer.group_discard(
+    def disconnect(self, code):
+        async_to_sync(self.channel_layer.group_discard)(
             self.question_group_name,
             self.channel_name
         )
 
-    async def receive(self, text_data):
+    def receive(self, text_data):
         text_data_json = json.loads(text_data)
         answer = text_data_json['text']
 
-        new_answer = await self.create_new_answer(answer)
+        new_answer = self.create_new_answer(answer)
         data = {
             'author': new_answer.author.username,
-            'date_published': new_answer.date_published.strftime('%Y-%m-%d'),
-            'content': new_answer.content
+            'date_published': new_answer.date_published.strftime('%d-%m-%Y'),
+            'content': new_answer.content,
+            'author_url': new_answer.author.profile.get_absolute_url(),
+            'profile_image': new_answer.author.profile.get_profile_image()
         }
+        print(data)
 
-        await self.channel_layer.group_send(
+        async_to_sync(self.channel_layer.group_send)(
             self.question_group_name,
             {
                 'type': 'new_answer',
@@ -44,18 +49,14 @@ class AnswersConsumer(AsyncWebsocketConsumer):
             }
         )
 
-    async def new_answer(self, event):
+    def new_answer(self, event):
         message = event['message']
+        self.send(text_data=json.dumps(
+            {
+                'message': message
+            }
+        ))
 
-        await self.send(
-            text_data=json.dumps(
-                {
-                    'message': message
-                }
-            )
-        )
-
-    @database_sync_to_async
     def create_new_answer(self, text):
         question = Question.objects.get(id=int(self.question_id))
         new_answer = Answer.objects.create(
@@ -63,4 +64,15 @@ class AnswersConsumer(AsyncWebsocketConsumer):
             author=self.scope['user'],
             content=text
         )
+
+        # Send notification to question author
+        Notification.objects.create(
+                from_user=new_answer.author, 
+                to_user=question.author,
+                target_content_type=ContentType.objects.get_for_model(question),
+                target_object_id=question.id,
+                message=f'Пользователь {new_answer.author} ответил на вопрос {question}',
+                url=question.get_absolute_url()
+            )
+        
         return new_answer
