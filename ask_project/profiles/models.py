f79803f5286fdf526823c5987d42bc896aabb1c1
EternalTLD <shvejkin01@mail.ru>
2023-09-22 22:54:45 +0300

    add profile image and profile url

--- ask_project/profiles/models.py
+++ ask_project/profiles/models.py
@@ -1,11 +1,24 @@
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.urls import reverse
 
-from users.models import User
+from ask_project import settings
 
 
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
-    profile_image = models.ImageField(upload_to='avatars/%Y/%m/%d/', blank=True, null=True, verbose_name='Аватар')
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL, 
+        on_delete=models.CASCADE, 
+        related_name='profile',
+        verbose_name='Пользователь'
+    )
+    profile_image = models.ImageField(
+        upload_to='avatars/%Y/%m/%d/', 
+        blank=True, 
+        null=True, 
+        verbose_name='Аватар'
+    )
     rating = models.IntegerField(default=0, verbose_name='Рейтинг')
     city = models.CharField(max_length=25, blank=True, verbose_name='Город')
     country = models.CharField(max_length=25, blank=True, verbose_name='Страна')
@@ -18,3 +31,16 @@ class Profile(models.Model):
     def __str__(self) -> str:
         return f'Профиль пользователя {self.user.username}'
 
+    def get_profile_image(self):
+        if self.profile_image:
+            return self.profile_image.url
+        return settings.MEDIA_URL + 'avatars/default/avatar_default.png'
+    
+    def get_absolute_url(self):
+        return reverse('profiles:profile_detail', kwargs={'username': self.user.username})
+    
+
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
