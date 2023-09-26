15fa06801e4d9ad4efd92400b741171a4a61a57f
EternalTLD <shvejkin01@mail.ru>
2023-09-26 16:25:17 +0300

    rework create profile signal

--- ask_project/profiles/models.py
+++ ask_project/profiles/models.py
@@ -1,3 +1,5 @@
+from typing import Any
+
 from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
@@ -7,6 +9,7 @@ from ask_project import settings
 
 
 class Profile(models.Model):
+    """Profile model"""
     user = models.OneToOneField(
         settings.AUTH_USER_MODEL, 
         on_delete=models.CASCADE, 
@@ -31,16 +34,17 @@ class Profile(models.Model):
     def __str__(self) -> str:
         return f'Профиль пользователя {self.user.username}'
 
-    def get_profile_image(self):
+    def get_profile_image(self) -> str:
         if self.profile_image:
             return self.profile_image.url
         return settings.MEDIA_URL + 'avatars/default/avatar_default.png'
     
-    def get_absolute_url(self):
+    def get_absolute_url(self) -> str:
         return reverse('profiles:profile_detail', kwargs={'username': self.user.username})
     
 
 @receiver(post_save, sender=settings.AUTH_USER_MODEL)
-def create_profile(sender, instance, created, **kwargs):
-    if created:
+def create_profile(**kwargs: Any) -> None:
+    instance = kwargs.pop('instance')
+    if kwargs.pop('created'):
         Profile.objects.create(user=instance)
