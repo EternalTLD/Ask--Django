0faa73196319d6e1d04f2811ce3b863e0d445ac4
EternalTLD <shvejkin01@mail.ru>
2024-01-02 23:37:57 +0300

    Changed urls names

--- ask_project/questions/urls.py
+++ ask_project/questions/urls.py
@@ -5,12 +5,13 @@ from . import views
 app_name = "questions"
 
 urlpatterns = [
-    path("search/", views.question_search_view, name="question_search"),
-    path("tags/<slug:tag_slug>/", views.QuestionsByTagListView.as_view(), name="by_tag"),
-    path("popular/", views.PopularQuestionsListView.as_view(), name="popular_questions"),
-    path("question/create/", views.QuestionCreateView.as_view(), name="question_create"),
-    path("<int:pk>/<slug:slug>/", views.QuestionView.as_view(), name="question_detail"),
-    path("update/<int:pk>/", views.QuestionUpdateView.as_view(), name="question_update"),
-    path("delete/<int:pk>/", views.QuestionDeleteView.as_view(), name="question_delete"),
     path("", views.QuestionsListView.as_view(), name="home"),
+    path("questions/popular/", views.PopularQuestionsListView.as_view(), name="popular"),
+    path("questions/by_tag/<slug:tag_slug>/", views.QuestionsByTagListView.as_view(), name="by_tag"),
+    path("questions/search/", views.question_search_view, name="search"),
+
+    path("question/<int:pk>/<slug:slug>/", views.QuestionView.as_view(), name="detail"),
+    path("question/create/", views.QuestionCreateView.as_view(), name="create"),
+    path("question/update/<int:pk>/", views.QuestionUpdateView.as_view(), name="update"),
+    path("question/delete/<int:pk>/", views.QuestionDeleteView.as_view(), name="delete"),
 ]
