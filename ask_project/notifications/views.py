872cb896088d0f9fe64e2b7e6631f40332c9eb51
EternalTLD <shvejkin01@mail.ru>
2024-01-05 23:06:56 +0300

    Added login required and post required decorators

--- ask_project/notifications/views.py
+++ ask_project/notifications/views.py
@@ -1,22 +1,19 @@
-from typing import Any
-from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseRedirect
 from django.db.models.query import QuerySet
 from django.shortcuts import get_object_or_404, redirect
 from django.views.generic.list import ListView
 from django.contrib.auth.decorators import login_required
+from django.views.decorators.http import require_POST
 from django.utils.decorators import method_decorator
 
 from .models import Notification
 
 
+@method_decorator(login_required, name="dispatch")
 class NotificationsListView(ListView):
     template_name = "notifications/notifications.html"
     context_object_name = "notifications"
     paginate_by = 20
-
-    @method_decorator(login_required)
-    def dispatch(self, request: HttpRequest, *args: Any, **kwargs: Any) -> HttpResponse:
-        return super(NotificationsListView, self).dispatch(request, *args, **kwargs)
 
 
 class AllNotificationsListView(NotificationsListView):
@@ -31,16 +28,16 @@ class UnreadNotificationsListView(NotificationsListView):
         return notifications
 
 
+@require_POST
+@login_required
 def mark_as_read_view(request, id: int) -> HttpResponseRedirect:
-    if request.method == "POST":
-        notification = get_object_or_404(Notification, id=id, to_user=request.user)
-        notification.mark_as_read()
-
+    notification = get_object_or_404(Notification, id=id, to_user=request.user)
+    notification.mark_as_read()
     return redirect("notifications:unread")
 
 
+@require_POST
+@login_required
 def mark_all_as_read_view(request) -> HttpResponseRedirect:
-    if request.method == "POST":
-        request.user.received_notifications.mark_all_as_read()
-
+    request.user.received_notifications.mark_all_as_read()
     return redirect("notifications:unread")
