4b60132491cfac6785e75fe9a5575a94b4ae4ed0
EternalTLD <shvejkin01@mail.ru>
2024-01-07 00:01:55 +0300

    Added URI field mixin and updated vote field mixin

--- ask_project/api/mixins.py
+++ ask_project/api/mixins.py
@@ -2,11 +2,39 @@ from rest_framework import serializers
 
 
 class VoteCountFieldMixin(metaclass=serializers.SerializerMetaclass):
+    """Mixin adds likes and dislikes counter fields"""
+
     likes = serializers.SerializerMethodField()
     dislikes = serializers.SerializerMethodField()
 
     def get_likes(self, instance):
-        return instance.votes.count_likes()
-    
+        return self._get_count(instance.votes, "likes")
+
     def get_dislikes(self, instance):
-        return instance.votes.count_dislikes()
+        return self._get_count(instance.votes, "dislikes")
+
+    def _get_count(self, instance, method_name):
+        try:
+            return getattr(instance, f"count_{method_name}")()
+        except AttributeError as exception:
+            raise serializers.ValidationError(
+                f"Error counting {method_name}: {exception}"
+            )
+
+
+class URIFieldMixin(metaclass=serializers.SerializerMetaclass):
+    """Mixin builds an URI of object if object has url field or get_absolute_url method"""
+
+    URI = serializers.SerializerMethodField()
+
+    def get_URI(self, instance):
+        model = self.context["view"].get_serializer().Meta.model
+        try:
+            if hasattr(model, "get_absolute_url"):
+                return self.context["request"].build_absolute_uri(
+                    instance.get_absolute_url()
+                )
+            elif hasattr(model, "url"):
+                return self.context["request"].build_absolute_uri(instance.url)
+        except (AttributeError, ValueError) as exception:
+            raise serializers.ValidationError(f"Error building URI: {exception}")
