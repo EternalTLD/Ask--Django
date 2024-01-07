264687a22a723b464f7e6572b2d4dd4ca2816d4a
EternalTLD <shvejkin01@mail.ru>
2024-01-07 20:56:18 +0300

    Added router

--- ask_project/api/urls.py
+++ ask_project/api/urls.py
@@ -1,43 +1,16 @@
-from django.urls import path
+from rest_framework import routers
+from django.urls import path, include
 
 from . import views
 
 
 app_name = "api"
 
+router = routers.SimpleRouter()
+router.register("users", views.UserViewSet)
+router.register("questions", views.QuestionViewSet)
+router.register("answers", views.AnswerViewSet)
+
 urlpatterns = [
-    path("users/", views.UserViewSet.as_view({"get": "list"})),
-    path(
-        "users/<int:pk>/",
-        views.UserViewSet.as_view(
-            {"get": "retrieve", "put": "update", "patch": "partial_update"}
-        ),
-    ),
-    path(
-        "questions/", views.QuestionViewSet.as_view({"get": "list", "post": "create"})
-    ),
-    path(
-        "questions/<int:pk>/",
-        views.QuestionViewSet.as_view(
-            {
-                "get": "retrieve",
-                "put": "update",
-                "patch": "partial_update",
-                "delete": "destroy",
-            }
-        ),
-    ),
-    path("answers/", views.AnswerViewSet.as_view({"get": "list"})),
-    path(
-        "answers/<int:pk>/",
-        views.AnswerViewSet.as_view(
-            {
-                "get": "retrieve",
-                "put": "update",
-                "patch": "partial_update",
-                "delete": "destroy",
-            }
-        ),
-    ),
-    path("notifications/", views.NotificationListAPIView.as_view()),
+    path("", include(router.urls)),
 ]
