3ad9c510b51e372e00e699765d8a25f190ea8a20
EternalTLD <shvejkin01@mail.ru>
2024-01-07 20:57:42 +0300

    Added is author permission

--- ask_project/api/permissions.py
+++ ask_project/api/permissions.py
@@ -5,12 +5,19 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 
-class IsAuthorOrReadOnly(permissions.BasePermission):
+class IsAuthor(permissions.BasePermission):
+    """Permission to only allow author of an object to get access"""
+
+    def has_object_permission(self, request, view, obj):
+        if isinstance(obj, User):
+            return request.user == obj
+        return request.user == obj.author
+
+
+class IsAuthorOrReadOnly(IsAuthor):
     """Permission to only allow author of an object to edit it"""
 
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return True
-        if isinstance(obj, User):
-            return request.user == obj
-        return request.user == obj.author
+        return super().has_object_permission(request, view, obj)
