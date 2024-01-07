38136e49dc96f40e6b78a4920b4b930498886663
EternalTLD <shvejkin01@mail.ru>
2024-01-07 20:57:01 +0300

    Added actions to user and question viewsets

--- ask_project/api/views.py
+++ ask_project/api/views.py
@@ -1,10 +1,12 @@
-from rest_framework import viewsets, permissions, generics
+from rest_framework import viewsets, permissions, status
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from django.contrib.auth import get_user_model
 
 from questions.models import Question, Answer
 from notifications.models import Notification
 from . import serializers
-from .permissions import IsAuthorOrReadOnly
+from .permissions import IsAuthorOrReadOnly, IsAuthor
 
 
 User = get_user_model()
@@ -15,11 +17,26 @@ class QuestionViewSet(viewsets.ModelViewSet):
     serializer_class = serializers.QuestionSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsAuthorOrReadOnly]
 
-    def retrieve(self, request, *args, **kwargs):
+    @action(
+        detail=True,
+        methods=["post"],
+        serializer_class=serializers.AnswerSerializer,
+        permission_classes=[permissions.IsAuthenticated],
+    )
+    def add_answer(self, request, pk):
         question = self.get_object()
-        question.views += 1
-        question.save()
-        return super().retrieve(request, *args, **kwargs)
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(question=question, author=request.user)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    @action(detail=True, methods=["get"])
+    def similar_questions(self, request, pk):
+        question = self.get_object()
+        similar_questions = Question.published.get_similar_questions(question)
+        serializer = self.get_serializer(similar_questions, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -27,17 +44,48 @@ class UserViewSet(viewsets.ModelViewSet):
     serializer_class = serializers.UserSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsAuthorOrReadOnly]
 
+    @action(
+        detail=True, methods=["get"], serializer_class=serializers.QuestionSerializer
+    )
+    def published_questions(self, request, pk):
+        user = self.get_object()
+        questions = Question.published.filter(author=user)
+        serializer = self.get_serializer(questions, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    @action(
+        detail=True,
+        methods=["get"],
+        serializer_class=serializers.QuestionSerializer,
+        permission_classes=[IsAuthor],
+    )
+    def all_questions(self, request, pk):
+        user = self.get_object()
+        questions = Question.objects.filter(author=user)
+        serializer = self.get_serializer(questions, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    @action(detail=True, methods=["get"], serializer_class=serializers.AnswerSerializer)
+    def answers(self, request, pk):
+        user = self.get_object()
+        answers = Answer.objects.filter(author=user)
+        serializer = self.get_serializer(answers, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    @action(
+        detail=True,
+        methods=["get"],
+        serializer_class=serializers.NotificationSerializer,
+        permission_classes=[IsAuthor],
+    )
+    def notifications(self, request, pk):
+        user = self.get_object()
+        notifications = Notification.objects.filter(to_user=user)
+        serializer = self.get_serializer(notifications, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
 
 class AnswerViewSet(viewsets.ModelViewSet):
     queryset = Answer.objects.all()
     serializer_class = serializers.AnswerSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsAuthorOrReadOnly]
-
-
-class NotificationListAPIView(generics.ListAPIView):
-    queryset = Notification.objects.all()
-    serializer_class = serializers.NotificationSerializer
-
-    def get_queryset(self):
-        notifications = Notification.objects.filter(to_user=self.request.user)
-        return notifications
