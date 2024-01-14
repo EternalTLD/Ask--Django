1c1a3bd2f86e5a2f378b623767ec8a8cc7f10a88
EternalTLD <shvejkin01@mail.ru>
2024-01-14 22:38:09 +0300

    Added entrypoint for web app

---
+++ ask_project/entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+echo "Collect static files"
+python manage.py collectstatic --noinput
+
+echo "Apply database migrations"
+python manage.py makemigrations
+python manage.py migrate
+
+exec "$@"
